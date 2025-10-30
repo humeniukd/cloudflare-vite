48ed074975ae85a508ae22880c2b646ce96f2998
Dimon <dmitry.gumenyuk@gmail.com>
2025-10-30 02:07:15 +0100

    working

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-
-export default {
-  darkMode: 'selector',
-  content: './src/**/*.{html,hbs}',
-  theme: {
-    extend: {},
-  },
-}
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import { cloudflare } from '@cloudflare/vite-plugin';
 import tailwindcss from '@tailwindcss/vite'
+import crypto from 'node:crypto';
 import { resolve } from 'node:path';
 import hb from 'vite-plugin-handlebars';
 import { pageData } from './pageData';
@@ -15,13 +16,26 @@ const props = `
     nonce=''
 `;
 
+const cssEntry = "index.css";
+
+function generateHash(data) {
+    const hash = crypto.createHash('SHAKE256', { outputLength: 4 })
+    hash.update(data);
+    return hash.digest('hex');
+}
+
 function handlebars(options) {
     const { transformIndexHtml, ...rest } = hb(options);
     return {
         ...rest,
         async transform(src, id) {
             if (/\.hbs$/.test(id)) {
-                const html = await transformIndexHtml.handler(src, { path: id })
+
+                let html = await transformIndexHtml.handler(src, { path: id });
+                if (process.env.NODE_ENV === "production") {
+                    const hash = process.env.CSS = generateHash(html);
+                    html = html.replace(cssEntry, `${hash}.css`);
+                }
                 return `export const template = ({${props}}) => \`${html}\``;
             }
         },
@@ -30,11 +44,23 @@ function handlebars(options) {
 
 export default defineConfig({
     plugins: [
+        cloudflare(),
         handlebars({
             context: pagePath => pageData[pagePath.split('/').pop()],
             partialDirectory: resolve(__dirname, 'src/partials'),
         }),
-        tailwindcss(),
-        cloudflare()
+        tailwindcss()
     ],
+    build: {
+        rollupOptions: {
+            input: cssEntry,
+            output: {
+                assetFileNames: (assetInfo) => {
+                    if (assetInfo.names.some(n => n === cssEntry))
+                        return process.env.CSS + ".css";
+                    return null;
+                },
+            },
+        },
+    }
 });
